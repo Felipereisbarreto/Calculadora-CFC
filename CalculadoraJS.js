fe60d4d2e1bc2865fe20a0514f84072f162f2d13
Felipe Barreto <felipereisbarreto@gmail.com>
2019-03-15 17:04:26 -0300

    inserindo calculo de raiz quadrada

--- CalculadoraJS.js
+++ CalculadoraJS.js
@@ -5,19 +5,45 @@ function calculadora(valorA, valorB, op){
 		return valorA / valorB;}
 	else if  (op === '+'){
 		return valorA + valorB;}
-
-}
+		else if  (op === 'raiz'){
+		return Math.pow(document.calculadora.fname.value, 0.5);}
+		}
 
 
 function numeros()
+	
 	{
 		  var numero1 = parseFloat(document.calculadora.fname.value);
 		  var numero2 = parseFloat(document.calculadora.lname.value);
 		  var operacao = (document.calculadora.operacao.value);
-		
+		  var calc = (document.calculadora.operacao.value)
+
+		 		
 		  var resultado = calculadora(numero1, numero2, operacao);
 
 		  document.calculadora.result.value = resultado;
+			
+			
+		if (isNaN(numero1) === true){
+	alert("Insira o valor 1");
+}
+else if (operacao == 'raiz'){
+ alert("Calculo Raiz Quadrada");}
 
-	      return resultado;
+else if (isNaN(numero2) === true){
+ alert("Insira o valor 2");
+}
+
+else if (operacao == false){
+ alert("Insira a operacao");
+}
+ else if (operacao == 'raiz'){
+ alert("Cálculo Raiz Quadrada");
+
+	      
 	}
+
+
+
+}
+		
