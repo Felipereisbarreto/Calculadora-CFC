61590c7afc299abf1070f28fdace5a9164495084
Felipe Barreto <felipereisbarreto@gmail.com>
2019-03-19 16:02:44 -0300

    colocando historico na calculadora

--- CalculadoraJS.js
+++ CalculadoraJS.js
@@ -5,45 +5,47 @@ function calculadora(valorA, valorB, op){
 		return valorA / valorB;}
 	else if  (op === '+'){
 		return valorA + valorB;}
+		else if (op === '*'){
+			return valorA * valorB;}
+			else if (op === '%'){
+			return  (valorA * valorB)/100;}
+
 		else if  (op === 'raiz'){
 		return Math.pow(document.calculadora.fname.value, 0.5);}
 		}
-
-
-function numeros()
-	
-	{
+var listahistorico = [];
+function numeros(){
 		  var numero1 = parseFloat(document.calculadora.fname.value);
 		  var numero2 = parseFloat(document.calculadora.lname.value);
 		  var operacao = (document.calculadora.operacao.value);
-		  var calc = (document.calculadora.operacao.value)
-
-		 		
+		  var calc = (document.calculadora.operacao.value);	
 		  var resultado = calculadora(numero1, numero2, operacao);
-
 		  document.calculadora.result.value = resultado;
+		  document.calculadora.resul2.value = resultado;
+		  
+
 			
-			
-		if (isNaN(numero1) === true){
-	alert("Insira o valor 1");
+	if (operacao == 'raiz'){
+document.calculadora.resul2.value = "A Raiz Quadrada de " + numero1 + " É = " + resultado;   
+historico("A Raiz Quadrada de " + numero1 + " É = " + resultado);   
 }
-else if (operacao == 'raiz'){
- alert("Calculo Raiz Quadrada");}
-
-else if (isNaN(numero2) === true){
- alert("Insira o valor 2");
+	else{
+document.calculadora.resul2.value = numero1 + operacao + numero2 + " = " + resultado;
+historico(numero1 + operacao + numero2 + " = " + resultado + "  ");	  	 
 }
-
-else if (operacao == false){
- alert("Insira a operacao");
+	if (isNaN(numero1) === true){
+alert("Insira um numero valido");
 }
- else if (operacao == 'raiz'){
- alert("Cálculo Raiz Quadrada");
-
-	      
-	}
-
-
-
+	else if (operacao == 'raiz'){
+alert("Calculo Raiz Quadrada");
 }
-		
+	else if (isNaN(numero2) === true){
+alert("Insira um numero valido");
+}
+	else if (operacao == false){
+alert("Insira a operacao");
+}
+	 else if (operacao == 'raiz'){
+alert("Calculo Raiz Quadrada");      
+}
+}
